b77d949fdfa91c186115d7dcdf2c759a282198b0
Adriana Ku <59809225+Adriana-ku06@users.noreply.github.com>
2020-03-04 21:23:21 -0600

    Add files via upload

---
+++ primenumber.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+#include<math.h>
+
+int main(){
+	int i=0;
+	int cont=0;
+	int j=0;
+	int cantp=0;
+for(i=1;i<=200;i++){
+	cont=0;
+	for(j=1;j<=i;j++){
+        
+		if(i%j==0){
+		cont++;
+			}
+	
+		}
+	if(cont==2)
+	{
+	printf("%d\n",i);
+
+		cantp++;
+}
+	}
+
+	printf("Existen %d primos", cantp);
+	
+}
